3ccb7c55b5c163d3b5339da6f11a00a5159c5a84
Julio-Cesar-Castro <Juliocastro.contato@gmail.com>
2025-03-20 14:30:23 -0300

    Test deploy

--- scritp.js
+++ scritp.js
@@ -1,59 +1,44 @@
 // DOM
 
-const input = document.querySelector("#input-list")
 const form = document.querySelector("form")
 const button = document.querySelector("#btn-add")
-const inputList = document.querySelector("#markAsRead")
-const listBox = document.querySelector("main")
+const listContainer = document.querySelector("main")
+
 
 // Build Structure
-
-const list = document.createElement("div")
-list.classList.add("card")
-
-const checkBox = document.createElement("input")
-checkBox.setAttribute("type", "checkbox")
-checkBox.setAttribute("name", "markAsRead")
-
-const paragraph = document.createElement("p")
-
-const removeIcon = document.createElement("img")
-removeIcon.setAttribute("src", "assets/icons/trash.svg")
-
-// Build Structure End
-
-  // list.appendChild(checkBox)
-  // list.appendChild(paragraph)
-  // list.appendChild(removeIcon)
 
 form.onsubmit = (e) => {
   e.preventDefault();
 
   button.addEventListener("click", () => {
+    console.log(BuildHTML())
+    listContainer.appendChild(BuildHTML())
 
-    listBox.append(buildList(list))
-    paragraph.innerText = input.value
   })
 }
 
-// functions
 
-/**
- * 
- * @param listBuilder Build a HTML code the structure comes like 
- * <div class="card">
- *  <input type="checkbox" name="markAsRead" />
- *  <p> Inner are rescue by what user typed </p>
- *  <img src= source from local file, icon to delete item from list></img>
- * </div> 
- * @returns the listBuilder
- */
-function buildList(listBuilder){
+function BuildHTML(){
+const input = document.querySelector("#input-list")
 
-  list.appendChild(checkBox)
-  list.appendChild(paragraph)
-  list.appendChild(removeIcon)
+const item = document.createElement("div")
 
+const checkBox = document.createElement("input")
 
-  return listBuilder
+const paragraph = document.createElement("p")
+paragraph.innerText = input.value
+
+const removeIcon = document.createElement("img")
+
+// Attributes
+item.classList.add("card")
+checkBox.setAttribute("type", "checkbox")
+removeIcon.setAttribute("src", "assets/icons/trash.svg")
+
+item.appendChild(checkBox)
+item.appendChild(paragraph)
+item.appendChild(removeIcon)
+
+return item
+
 }
