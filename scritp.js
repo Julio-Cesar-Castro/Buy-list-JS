3e9d82d244c4dc2f12a08d6d3ffaec57cb45a501
Julio-Cesar-Castro <Juliocastro.contato@gmail.com>
2025-03-20 19:38:02 -0300

    Project done

--- scritp.js
+++ scritp.js
@@ -10,25 +10,48 @@ const listContainer = document.querySelector("main")
 form.onsubmit = (e) => {
   e.preventDefault();
 
-  button.addEventListener("click", () => {
-    console.log(BuildHTML())
-    listContainer.appendChild(BuildHTML())
-
-  })
+  listContainer.appendChild(BuildHTML())
 }
 
 
 function BuildHTML(){
 const input = document.querySelector("#input-list")
 
+if (input.value === ""){
+  alert('Digite um item para adicionar a lista de compras')
+  return
+}
+
 const item = document.createElement("div")
 
 const checkBox = document.createElement("input")
+
+checkBox.addEventListener('click', (e) => {
+  let isMarked = e.target.checked
+
+  if (isMarked) {
+    paragraph.style.textDecoration = 'line-through'
+  } else {
+    paragraph.style.textDecoration = 'initial';
+  }
+})
 
 const paragraph = document.createElement("p")
 paragraph.innerText = input.value
 
 const removeIcon = document.createElement("img")
+
+removeIcon.addEventListener('click', () => {
+  const message = document.querySelector("#alert")
+
+  message.addEventListener('click', () => {
+    message.remove(message)
+  })
+
+  item.remove(item)
+
+  message.classList.remove("hidden")
+})
 
 // Attributes
 item.classList.add("card")
@@ -38,6 +61,7 @@ removeIcon.setAttribute("src", "assets/icons/trash.svg")
 item.appendChild(checkBox)
 item.appendChild(paragraph)
 item.appendChild(removeIcon)
+
 
 return item
 
